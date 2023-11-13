b77ecd826292e0e0c0acb62dcd16d03fcbd8a5d1
Moukarnold <moukarnold@gmail.com>
2023-11-13 20:10:16 +0100

    display the total amount on the cart usinf useref

--- src/components/Meals/MealsItem/cardMealForm.jsx
+++ src/components/Meals/MealsItem/cardMealForm.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
 import "./mealItem.css";
 import Input from "../../UI/Input";
  
 
-function CardMealForm(){
+function CardMealForm(props){
+
+ const [amountIsValid, setAmountIsValid]= useState(true);
+  const amountInputRef= useRef();
+   
+
+   function submitHandler(event){
+      event.preventDefault();
+
+        //enteredAmount is a string
+     const enteredAmount= amountInputRef.current.value;
+      const enterdAmountNumber= +enteredAmount;
+
+      if(enteredAmount.trim().length===0 || enteredAmount<1 || enterdAmountNumber>5){
+
+        setAmountIsValid(false);
+        return;
+      }
+      props.onAddToCart(enterdAmountNumber);
+   };
+
 
 return (
-<form className="form">
-  <Input label = "Amount " input={{
+<form className="form" onSubmit={submitHandler}>
+  <Input label = "Amount" ref={amountInputRef} input={{
     id:"amount",
     type:"number",
     min:"1",
@@ -16,6 +37,7 @@ return (
     defaultValue:"1"
   }}/>
  <button> Add </button> 
+ {!amountIsValid && <p> Please enter a valid amount (1 - 5)</p>}
 </form>
 )
 
