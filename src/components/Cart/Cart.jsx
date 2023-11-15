ce8d8ae466f8764025ff0835ee99b19972efb86d
Moukarnold <moukarnold@gmail.com>
2023-11-15 14:06:26 +0100

    outputting cart Items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import Modal from "../UI/Modal";
 import "./Cart.css";
 import CartContext from "../../store/CartContext";
+import CartItem from "./CartItem";
 
 function Cart(props){
  const cartCtx = useContext(CartContext);
@@ -10,10 +11,24 @@ function Cart(props){
      const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
    const hasItems= cartCtx.items.length > 0;
 
+    function cartItemRemoveHandler(id){
+
+    };
+
+    function cartItemAddHandler(item){
+
+    };
+
       const cartItems= (
         <ul className="cart-item">
        { cartCtx.items.map((item)=>
-         ( <li> {item.name}</li>)
+         ( <li> <CartItem  
+          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onRemove={cartItemRemoveHandler.bind(null, item)}
+           key={item.id}
+            name={item.name} 
+            amount={item.amount} 
+            price={item.price}/></li>)
         )}
         </ul>)
 
@@ -27,7 +42,7 @@ function Cart(props){
        </div>
        <div className="actions"> 
         <button className="button--alt" onClick={props.onClose}> Close</button>
-      { hasItems&& <button className="button" onClick={props.onClose}> Order</button>}
+      { hasItems && <button className="button" onClick={props.onClose}> Order</button>}
        </div>
        </Modal>
         </>
